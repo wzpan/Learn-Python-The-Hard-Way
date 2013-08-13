2fef31a92d8e8e8e1ed04057db598b7d9fbe83df
Joseph Pan <cs.wzpan@gmail.com>
2013-08-13 22:06:19 +0000

    Make the script print only one of the lines.

--- Python2/ex01.py
+++ Python2/ex01.py
@@ -4,10 +4,3 @@
 # ex01: A Good First Program
 
 print "Hello World!"
-print "Hello Again"
-print "I like typing this."
-print "This is fun."
-print 'Yay! Printing.'
-print "I'd much rather you 'not'."
-# print 'I "said" do not touch this.' 
-print 'This is another line!'
