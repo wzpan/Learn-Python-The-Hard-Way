01b3bf8acf24343d3a9ca88b7ab87818b7ab7783
Joseph Pan <cs.wzpan@gmail.com>
2013-08-13 21:56:58 +0000

    add ex1 for python3

---
+++ Python3/ex01.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+
+# ex01: A Good First Program
+
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.') 
