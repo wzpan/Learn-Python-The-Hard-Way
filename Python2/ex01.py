04d94bb0b3b3d7a2406d7e211d4267aeec05caf1
Joseph Pan <cs.wzpan@gmail.com>
2013-08-13 21:54:10 +0000

    add ex1

---
+++ Python2/ex01.py
@@ -0,0 +1,12 @@
+#!/bin/python2
+# -*- coding: utf-8 -*-
+
+# ex01: A Good First Program
+
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.' 
