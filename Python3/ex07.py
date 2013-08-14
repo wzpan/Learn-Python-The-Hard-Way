9b225cbb9afc33734898a69ecd22be941c5099f1
Joseph Pan <cs.wzpan@gmail.com>
2013-08-14 10:17:32 +0000

    comment each line of ex7

--- Python3/ex07.py
+++ Python3/ex07.py
@@ -2,11 +2,19 @@
 
 # ex7: More Printing
 
+# Print "Mary had a little lamb" 
 print("Mary had a little lamb")
-print("Its fleece was white as %s." % 'snow')
-print("And everywhere that Mary went.")
-print("." * 10)  # what'd that do?
 
+# Print "Its fleece was white as snow.
+print("Its fleece was white as %s." % 'snow')  # one string inside of another
+
+# Print "And everywhere that Mary went."
+print("And everywhere that Mary went.")
+
+# Print ".........."
+print("." * 10)  # what'd that do? - "*" operator for strings is used to repeat the same characters for certain times
+
+# Assign string value for each variable
 end1 = "C"
 end2 = "h"
 end3 = "e"
@@ -24,5 +32,5 @@ end11 = "e"
 end12 = "r"
 
 # watch the first statement at the end. try removing it to see what happens
-print(end1 + end2 + end3 + end4 + end5 + end6, end=" ")
+print(end1 + end2 + end3 + end4 + end5 + end6, end = " ")
 print(end7 + end8 + end9 + end10 + end11 + end12)
