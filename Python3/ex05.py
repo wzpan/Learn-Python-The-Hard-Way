815340d3e75601aabbbe408c2d688879b07a9262
Joseph Pan <cs.wzpan@gmail.com>
2013-08-14 09:37:43 +0000

    add ex5

---
+++ Python3/ex05.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+# ex5: More Variables and Printing
+
+my_name = 'Zed A. Shaw'
+my_age = 35      # not a lie
+my_height = 74   # inches
+my_weight = 180  # lbs
+my_eyes = "Blue"
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print("Let's talk about %s" % my_name)
+print("He's %d inches tall." % my_height)
+print("He\'s %d pounds heavy." % my_weight)
+print("He's got %s my_eyes and %s my_hair." % (my_eyes, my_hair))
+print("Actually that's not too heavy")
+print("His my_teeth are usually %s depending on the coffee." % my_teeth)
+
+# this line is tricky, try to get it exactly right
+print('If I add %d, %d and %d I get %d.' % (my_age, my_height, my_weight, my_age + my_height + my_weight))
