71c7d2b12c12f0d20c8f5852b462803ae87648a1
Joseph Pan <cs.wzpan@gmail.com>
2013-08-14 11:10:42 +0000

    add ex14

---
+++ Python2/ex14.py
@@ -0,0 +1,26 @@
+#!/bin/python2
+# -*- coding: utf-8 -*-
+
+# ex14: Prompting and Passing
+
+from sys import argv
+
+script, user_name = argv
+prompt = '> '
+
+print "Hi %s, I'm the %s script." % (user_name, script)
+print "I'd like to ask you a few questions."
+print "Do you like me %s?" % user_name
+likes = raw_input(prompt)
+
+print "Where do you live %s?" % user_name
+lives = raw_input(prompt)
+
+print "What kind of computer do you have?"
+computer = raw_input(prompt)
+
+print """
+Alright, so you said %r about liking me.
+You live in %r.  Not sure where that is.
+And you have a %r computer.  Nice.
+""" % (likes, lives, computer)
---
+++ Python3/ex14.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+# ex14: Prompting and Passing
+
+from sys import argv
+
+script, user_name = argv
+prompt = '>'
+
+print("Hi %s, I'm the %s script." % (user_name, script))
+print("I'd like to ask you a few questions.")
+print("Do you like me %s?" % user_name)
+likes = input(prompt)
+
+print("Where do you live %s?" % user_name)
+lives = input(prompt)
+
+print("What kind of computer do you have?")
+computer = input(prompt)
+
+print("""
+Alright, so you said %r about liking me.
+You live in %r.  Not sure where that is.
+And you have a %r computer.  Nice.
+""" % (likes, lives, computer))
