10dc085c1532a44ec1ab58b715362cf07a3a01a7
Joseph Pan <cs.wzpan@gmail.com>
2013-08-14 10:35:22 +0000

    Use triple-single-quote instead

--- Python3/ex10.py
+++ Python3/ex10.py
@@ -7,12 +7,12 @@ persian_cat = "I'm split\non a line."
 backslash_cat = "I'm \\ a \\ cat."
 test = "I will insert a \newline haha."
 
-fat_cat = """
+fat_cat = '''
 I'll do a list:
 \t* Cat food
 \t* Fishies
 \t* Catnip \n\t* Grass
-"""
+'''
 
 print(tabby_cat)
 print(persian_cat)
