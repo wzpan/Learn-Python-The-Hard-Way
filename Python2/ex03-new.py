bdef4933b419c2ae03db6f63ca82a75d19430519
Joseph Pan <cs.wzpan@gmail.com>
2013-08-14 09:23:08 +0000

    write something to calculate on a new .py file.

---
+++ Python2/ex03-new.py
@@ -0,0 +1,31 @@
+#!/bin/python2
+# -*- coding: utf-8 -*-
+
+# Find something you need to calculate and write a new .py file that
+# does it.
+
+# integer
+print 50 * 2
+print 1/500
+print 4 * 3 - 1
+
+# float
+print 3.14 * 2 * 200
+print 1.0/20
+
+# The following expressions are more complicated calculations.
+# Ignore them if you haven't learned anything about each type.
+
+# decimal: more accurate than float
+# import decimal
+# print (decimal.Decimal(9876) + decimal.Decimal("54321.012345678987654321"))
+
+# fraction
+# import fractions
+# print fractions.Fraction(1, 3)
+# print fractions.Fraction(4, 6)
+# print 3 * fractions.Fraction(1, 3)
+
+# complex
+# print(1j * 1J)
+# print(3 + 1j * 3)j
