00f99661a1d687c9687a71892ebad48151d68b87
Joseph Pan <cs.wzpan@gmail.com>
2013-08-14 11:02:42 +0000

    add ex12

---
+++ Python2/ex12.py
@@ -0,0 +1,10 @@
+#!/bin/python2
+# -*- coding: utf-8 -*-
+
+# ex12: Prompting People
+
+age = raw_input("How old are you?")
+height = raw_input("How tall are you? ")
+weight = raw_input("How much do you weigh? ")
+
+print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
