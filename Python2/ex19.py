1dc0a7a578ca4edd58c18edc8a2a420c73d05b24
Joseph Pan <cs.wzpan@gmail.com>
2013-08-14 15:01:48 +0000

    write another function and run it 10 different ways

--- Python2/ex19.py
+++ Python2/ex19.py
@@ -38,3 +38,54 @@ print "And we can combine the two, variables and math:"
 # Call the function, with two expression that consists of variables
 # and math as the actual parameters
 cheese_and_crackers(amount_of_cheese + 100, amount_of_cheese + 1000)
+
+
+def print_args(*argv):
+    size = len(argv)
+    print size
+    print "Hello! Welcome to use %r!" % argv[0]
+    if size > 1:
+        for i in range(1, size):
+            print "The param %d is %r" % (i, argv[i])
+        return 0
+    return -1
+
+
+# 1. use numbers as actual parameters
+print_args(10, 20, 30)
+
+# 2. use string and numbers as actual parameters
+print_args("print_args", 10, 20)
+
+# 3. use strings as actual parameters
+print_args("print_args", "Joseph", "Pan")
+
+# 4. use variables as actual parameters
+first_name = "Joseph"
+last_name = "Pan"
+print_args("print_args", first_name, last_name)
+
+# 5. contain math expressions
+print_args("print_args", 5*4, 2.0/5)
+
+# 6. more complicated calculations
+print_args("print_args", '.'*10, '>'*3)
+
+# 7. more parameters
+print_args("print_args", 10, 20, 30, 40, 50)
+
+# 8. tuples as parameters
+nums1 = (10, 20, 30)
+nums2 = (40, 50, 60)
+print_args("print_args", nums1, nums2)
+
+# 9. more complicated types
+nums3 = [70, 80, 90]
+set1 = {"apple", "banana", "orange"}
+dict1 = {'id': '0001', 'name': first_name+" "+last_name}
+str1 = "Wow, so complicated!"
+print_args("print args", nums1, nums2, nums3, set1, dict1, str1)
+
+# 10. function as parameter and return values
+if print_args(cheese_and_crackers, print_args) != -1:
+    print "You just send more than one parameter. Great!"
