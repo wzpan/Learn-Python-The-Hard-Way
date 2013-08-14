25460b36504eb5486492fb99a06b828590243a88
Joseph Pan <cs.wzpan@gmail.com>
2013-08-14 11:33:15 +0000

    also do a close() on the txt and txt_again variables

--- Python3/ex15.py
+++ Python3/ex15.py
@@ -2,15 +2,33 @@
 
 # ex15: Reading Files
 
-# prompt to type the file name
-print("Type the filename:")
-# input the file name
-filename = input("> ")
+# import argv variables from sys submodule
+from sys import argv
 
-# open the selected file
+# get the argv variables
+script, filename = argv
+
+# open a file
 txt = open(filename)
 
 # print file name
 print("Here's your file %r: " % filename)
 # print all the contents of the file
 print(txt.read())
+
+# close the file
+txt.close()
+
+# prompt to type the file name again
+print("Type the filename again:")
+# input the new file name
+file_again = input("> ")
+
+# open the new selected file
+txt_again = open(file_again)
+
+# print the contents of the new selected file
+print(txt_again.read())
+
+# close the file
+txt_again.close()
