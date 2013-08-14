65dc7e30925871a8ecb26852aa05ec4e7aa807a7
Joseph Pan <cs.wzpan@gmail.com>
2013-08-14 11:37:21 +0000

    add ex16

---
+++ Python3/ex16.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+# ex16: Reading and Writing Files
+
+from sys import argv
+
+script, filename = argv
+
+from sys import argv
+
+script, filename = argv
+
+print("We're going to erase %r." % filename)
+print("If you don't want that, hit CTRL-C (^C).")
+print("If you do want that, hit RETURN.")
+
+input("?")
+
+print("Opening the file...")
+target = open(filename, 'w')
+
+print("Truncating the file.  Goodbye!")
+target.truncate()
+
+print("Now I'm going to ask you for three lines.")
+
+line1 = input("line 1: ")
+line2 = input("line 2: ")
+line3 = input("line 3: ")
+
+print("I'm going to write these to the file.")
+
+target.write(line1)
+target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write(line3)
+target.write("\n")
+
+print("And finally, we close it.")
+target.close()
