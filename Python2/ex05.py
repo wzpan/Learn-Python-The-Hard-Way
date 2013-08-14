656546218e2dae6f12f6b02516df0b55d18f56a5
Joseph Pan <cs.wzpan@gmail.com>
2013-08-14 09:42:47 +0000

    change all the variables so there isn't the my_ in front

--- Python2/ex05.py
+++ Python2/ex05.py
@@ -3,21 +3,21 @@
 
 # ex5: More Variables and Printing
 
-my_name = 'Zed A. Shaw'
-my_age = 35       # not a lie
-my_height = 74   # inches
-my_weight = 180  # lbs
-my_eyes = "Blue"
-my_teeth = 'White'
-my_hair = 'Brown'
+name = 'Zed A. Shaw'
+age = 35       # not a lie
+height = 74   # inches
+weight = 180  # lbs
+eyes = "Blue"
+teeth = 'White'
+hair = 'Brown'
 
-print "Let's talk about %s" % my_name
-print "He's %d years old." % my_age
-print "He's %d inches tall." % my_height
-print "He's %d pounds heavy." % my_weight
+print "Let's talk about %s" % name
+print "He's %d years old." % age
+print "He's %d inches tall." % height
+print "He's %d pounds heavy." % weight
 print "Actually that's not too heavy"
-print "He's got %s my_eyes and %s my_hair." % (my_eyes, my_hair)
-print "His my_teeth are usually %s depending on the coffee." % (my_teeth)
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % (teeth)
 
 # this line is tricky, try to get it exactly right
-print 'If I add %d, %d and %d I get %d.' % (my_age, my_height, my_weight, my_age + my_height + my_weight)
+print 'If I add %d, %d and %d I get %d.' % (age, height, weight, age + height + weight)
