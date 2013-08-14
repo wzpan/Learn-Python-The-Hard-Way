a67191d9a5c2877c13c1873e9acda33348632e15
Joseph Pan <cs.wzpan@gmail.com>
2013-08-14 10:58:52 +0000

    ask more questions

--- Python3/ex11.py
+++ Python3/ex11.py
@@ -17,3 +17,31 @@ print("So, you're %r old, %r tall and %r heavy." % (age, height, weight))
 print("Enter a integer: ", end=" ")
 num = int(raw_input())
 print("The number you've input is: %d" % num)
+
+print("Enter a name: ", end=" ")
+name = input()
+print("What's %s's age?" % name, end=" ")
+age = eval(input())
+print("What's %s's height?" % name, end=" ")
+height = eval(input())
+print("What's %s's weight?" % name, end=" ")
+weight = eval(input())
+print("What's the color of %s's eyes?" % name, end=" ")
+eyes = input()
+print("What's the color of %s's teeth?" % name, end=" ")
+teeth = input()
+print("What's the color of %s's hair?" % name, end=" ")
+hair = input()
+
+type(name)  # the data type of name will be <class 'str'>
+type(age)   # the data type of age will be <class 'int'>
+
+print("Let's talk about %s" % name)
+print("He's %d years old." % age)
+print("He's %d inches tall." % height)
+print("He's %d pounds heavy." % weight)
+print("Actually that's not too heavy")
+print("He's got %s eyes and %s hair." % (eyes, hair))
+print("His teeth are usually %s depending on the coffee." % (teeth))
+
+print('If I add %d, %d and %d I get %d.' % (age, height, weight, age + height + weight))
