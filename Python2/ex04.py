fa42ebe19bbe566287236592aaee14d7a646d4cb
Joseph Pan <cs.wzpan@gmail.com>
2013-08-14 09:29:25 +0000

    comment each line of ex4

--- Python2/ex04.py
+++ Python2/ex04.py
@@ -3,13 +3,21 @@
 
 # ex4: Variables And Names
 
+# number of cars
 cars = 100
+# space in a car. Give it a float value, otherwise we can only get int results after division.
 space_in_a_car = 4.0
+# number of drivers
 drivers = 30
+# number of passengers
 passengers = 90
+# number of cars that are empty(without driver)
 cars_not_driven = cars - drivers
+# number of cars that are diven
 cars_driven = drivers
+# number of cars 
 carpool_capacity = cars_driven * space_in_a_car
+# average number of passengers in each car
 average_passengers_per_car = passengers / cars_driven
 
 print "There are", cars, "cars available."
