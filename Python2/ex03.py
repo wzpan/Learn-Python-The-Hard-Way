54df2bffbd3e1ef4e1e224b4a4750ee8619f0dbb
Joseph Pan <cs.wzpan@gmail.com>
2013-08-14 09:23:46 +0000

    use floating point numbers so it's more accurate

--- Python2/ex03.py
+++ Python2/ex03.py
@@ -15,7 +15,7 @@ print "Roosters", 100 - 25 * 3 * 4
 print "Now I will count the eggs:"
 
 # Number of eggs, Notice that '/' operator returns int value in Python2
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print 3 + 2 + 1 - 5 + 4 % 2 - 1.0 / 4 + 6  # use floating point numbers so it's more accurate 
 
 # Print "Is it true that 3 + 2 < 5 - 7?"
 print "Is it true that 3 + 2 < 5 - 7?"
