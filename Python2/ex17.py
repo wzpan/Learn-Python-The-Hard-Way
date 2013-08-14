0219776bc131ce62335cc759b969f0da75078693
Joseph Pan <cs.wzpan@gmail.com>
2013-08-14 13:31:21 +0000

    make the core part one line long.

--- Python2/ex17.py
+++ Python2/ex17.py
@@ -8,18 +8,4 @@ from os.path import exists
 
 script, from_file, to_file = argv
 
-# we could do these two on one line too, how?
-in_file = open(from_file)
-indata = in_file.read()
-
-print "Does the output file exists? %r"  % exists(to_file)
-print "Ready, hit RETURN to continue, CTRL-C to abort."
-raw_input()
-
-out_file = open(to_file, 'w')
-out_file.write(indata)
-
-print "Alright, all done"
-
-out_file.close()
-in_file.close()
+open(to_file, 'w').write(open(from_file).read())
