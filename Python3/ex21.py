98d376dcb825f1c22ac5567a32e639291bf66866
Joseph Pan <cs.wzpan@gmail.com>
2013-08-14 21:13:31 +0000

    add a new function to use return

--- Python3/ex21.py
+++ Python3/ex21.py
@@ -22,6 +22,12 @@ def divide(a, b):
     return a / b
 
 
+# my function to test return
+def isequal(a, b):
+    print("Is %r equal to %r? - " % (a, b))
+    return (a == b)
+
+
 print("Let's do some math with just functions!")
 
 age = add(30, 5)
@@ -38,3 +44,12 @@ print("Here is a puzzle.")
 what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
 
 print("That becomes: ", what, "Can you do it by hand?")
+
+
+# test the return value of isequal()
+num1 = 40
+num2 = 50
+num3 = 50
+
+print(isequal(num1, num2))
+print(isequal(num2, num3))
