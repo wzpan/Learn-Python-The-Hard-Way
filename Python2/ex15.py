45e96110dce1030b7434383ba82a7982de92d571
Joseph Pan <cs.wzpan@gmail.com>
2013-08-14 11:22:30 +0000

    comment each line of ex15

--- Python2/ex15.py
+++ Python2/ex15.py
@@ -3,18 +3,27 @@
 
 # ex15: Reading Files
 
+# import argv variables from sys submodule
 from sys import argv
 
+# get the argv variables
 script, filename = argv
 
+# open a file
 txt = open(filename)
 
+# print file name
 print "Here's your file %r:" % filename
+# print all the contents of the file
 print txt.read()
 
+# prompt to type the file name again
 print "Type the filename again:"
+# input the new file name
 file_again = raw_input("> ")
 
+# open the new selected file
 txt_again = open(file_again)
 
+# print the contents of the new selected file
 print txt_again.read()
