e5a97d211b3c4753189f987cbbaf6c426adcaded
Joseph Pan <cs.wzpan@gmail.com>
2013-08-14 11:16:53 +0000

    add another argument

--- Python2/ex14.py
+++ Python2/ex14.py
@@ -5,24 +5,26 @@
 
 from sys import argv
 
-script, user_name = argv
-# Change the prompt variable to something else
+# Add another argument and use it in your script
+script, user_name, city = argv
 # prompt = '> '
+# Change the prompt variable to something else
 prompt = 'Please type the answer: '
 
-print "Hi %s, I'm the %s script." % (user_name, script)
+print "Hi %s from %s, I'm the %s script." % (user_name, city, script)
 print "I'd like to ask you a few questions."
 print "Do you like me %s?" % user_name
 likes = raw_input(prompt)
 
-print "Where do you live %s?" % user_name
-lives = raw_input(prompt)
+print "What's the whether like today in %s?" % city
+weather = raw_input(prompt)
 
 print "What kind of computer do you have?"
 computer = raw_input(prompt)
 
 print """
 Alright, so you said %r about liking me.
-You live in %r.  Not sure where that is.
+The weather in your city is %s. 
+But I can't feel it because I'm a robot.
 And you have a %r computer.  Nice.
-""" % (likes, lives, computer)
+""" % (likes, weather, computer)
