eb1f0c047f7197298287a93d1183bc6f093b340a
Joseph Pan <cs.wzpan@gmail.com>
2013-08-14 11:22:02 +0000

    add ex15

---
+++ Python2/ex15.py
@@ -0,0 +1,20 @@
+#!/bin/python2
+# -*- coding: utf-8 -*-
+
+# ex15: Reading Files
+
+from sys import argv
+
+script, filename = argv
+
+txt = open(filename)
+
+print "Here's your file %r:" % filename
+print txt.read()
+
+print "Type the filename again:"
+file_again = raw_input("> ")
+
+txt_again = open(file_again)
+
+print txt_again.read()
---
+++ Python3/ex15.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+
+# ex15: Reading Files
+
+from sys import argv
+
+script, filename = argv
+
+txt = open(filename)
+
+print("Here's your file %r: " % filename)
+print(txt.read())
+
+print("Type the filename again:")
+file_again = input("> ")
+
+txt_again = open(file_again)
+
+print(txt_again.read())
