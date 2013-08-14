2d86feb934e26ec4190beb781047ec9a67d6b57d
Joseph Pan <cs.wzpan@gmail.com>
2013-08-14 21:12:49 +0000

    add ex21

---
+++ Python2/ex21.py
@@ -0,0 +1,42 @@
+#!/bin/python2
+# -*- coding: utf-8 -*-
+
+# ex21: Functions Can Return Something
+
+
+def add(a, b):
+    print "ADDING %d + %d" % (a, b)
+    return a + b
+
+
+def subtract(a, b):
+    print "SUBTRACTING %d - %d" % (a, b)
+    return a - b
+
+
+def multiply(a, b):
+    print "MULTIPLYING %d * %d" % (a, b)
+    return a * b
+
+
+def divide(a, b):
+    print "DIVIDING %d / %d" % (a, b)
+    return a / b
+
+
+print "Let's do some math with just functions!"
+
+age = add(30, 5)
+height = subtract(78, 4)
+weight = multiply(92, 2)
+iq = divide(100, 2)
+
+print "Age: %d, Height: %d, Weight: %d, IQ: %d" % (age, height, weight, iq)
+
+
+# A puzzle for the extra credit, type it in anyway.
+print "Here is a puzzle."
+
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+
+print "That becomes: ", what, "Can you do it by hand?"
