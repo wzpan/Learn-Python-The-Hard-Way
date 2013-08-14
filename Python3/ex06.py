01619ba0da3be451391ad8603299cc7c1c30f96f
Joseph Pan <cs.wzpan@gmail.com>
2013-08-14 10:06:06 +0000

    add ex6

---
+++ Python3/ex06.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+# ex6: String and Text
+
+x = "There are %d types of people." % 10
+binary = "binary"
+do_not = "don't"
+y = "Those who know %s and those who %s." % (binary, do_not)
+
+print(x)
+print(y)
+
+print("I said %r." % x)
+print("I also said: '%s'." % y)
+
+hilarious = False
+joke_evaluation = "Isn't that joke so funny?! %r" 
+
+print(joke_evaluation % hilarious)
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+print(w + e)
