ba3f5b4cd090e4e1c9171c998128d27fd54d5362
Joseph Pan <cs.wzpan@gmail.com>
2013-08-14 11:04:18 +0000

    add ex13

---
+++ Python2/ex13.py
@@ -0,0 +1,13 @@
+#!/bin/python2
+# -*- coding: utf-8 -*-
+
+# ex13: Parameters, Unpacking, Variables
+
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
---
+++ Python3/ex13.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+# ex13: Parameters, Unpacking, Variables
+
+from sys import argv
+
+script, first, second, third = argv
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
