2d32dfd7bcc02e7bc566f4aa72bc553588bb03ba
Joseph Pan <cs.wzpan@gmail.com>
2013-08-15 15:23:39 +0000

    fix bugs

--- Python3/ex26.py
+++ Python3/ex26.py
@@ -1,3 +1,5 @@
+#!/bin/python3
+
 def break_words(stuff):
     """This function will break up words for us."""
     words = stuff.split(' ')
@@ -7,15 +9,20 @@ def sort_words(words):
     """Sorts the words."""
     return sorted(words)
 
-def print_first_word(words)
+# bug: def print_first_word(words)
+def print_first_word(words):
     """Prints the first word after popping it off."""
-    word = words.poop(0)
-    print word
+    # bug: word = words.poop(0)
+    word = words.pop(0)
+    # bug: print word
+    print(word)
 
 def print_last_word(words):
     """Prints the last word after popping it off."""
-    word = words.pop(-1
-    print word
+    # bug: word = words.pop(-1
+    word = words.pop(-1)
+    # bug: print word
+    print(word)
 
 def sort_sentence(sentence):
     """Takes in a full sentence and returns the sorted words."""
@@ -35,8 +42,10 @@ def print_first_and_last_sorted(sentence):
     print_last_word(words)
 
 
-print "Let's practice everything."
-print 'You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs.'
+# bug: print "Let's practice everything."
+print("Let's practice everything.")
+# bug: print 'You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs.'
+print('You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs.')
 
 poem = """
 \tThe lovely world
@@ -47,45 +56,64 @@ and requires an explantion
 \n\t\twhere there is none.
 """
 
+# bug: print "--------------"
+print("--------------")
+# bug: print poem
+print(poem)
+# bug: print "--------------"
+print("--------------")
 
-print "--------------"
-print poem
-print "--------------"
-
+# warning: it's not five
 five = 10 - 2 + 3 - 5
-print "This should be five: %s" % five
+# bug: print "This should be five: %s" % five
+print("This should be five: %s" % five)
+
 
 def secret_formula(started):
     jelly_beans = started * 500
-    jars = jelly_beans \ 1000
+    # bug: jars = jelly_beans \ 1000
+    jars = jelly_beans / 1000
     crates = jars / 100
     return jelly_beans, jars, crates
 
 
 start_point = 10000
-beans, jars, crates == secret_formula(start-point)
+# bug: beans, jars, crates == secret_formula(start-point)
+beans, jars, crates = secret_formula(start_point)
 
-print "With a starting point of: %d" % start_point
-print "We'd have %d jeans, %d jars, and %d crates." % (beans, jars, crates)
+# bug: print "With a starting point of: %d" % start_point
+print("With a starting point of: %d" % start_point)
+# bug: print "We'd have %d jeans, %d jars, and %d crates." % (beans, jars, crates)
+print("We'd have %d jeans, %d jars, and %d crates." % (beans, jars, crates))
 
 start_point = start_point / 10
 
-print "We can also do that this way:"
-print "We'd have %d beans, %d jars, and %d crabapples." % secret_formula(start_pont
+# bug: print "We can also do that this way:"
+print("We can also do that this way:")
+# bug: print "We'd have %d beans, %d jars, and %d crabapples." %
+# secret_formula(start_pont
+print("We'd have %d beans, %d jars, and %d crabapples." % secret_formula(start_point))
 
 
-sentence = "All god\tthings come to those who weight."
+# bug: sentence = "All god\tthings come to those who weight."
+sentence = "All good things come to those who weight."
 
-words = ex25.break_words(sentence)
-sorted_words = ex25.sort_words(words)
+# bug: words = ex25.break_words(sentence)
+words = break_words(sentence)
+# bug: sorted_words = ex25.sort_words(words)
+sorted_words = sort_words(words)
 
 print_first_word(words)
 print_last_word(words)
-.print_first_word(sorted_words)
+# bug: .print_first_word(sorted_words)
+print_first_word(sorted_words)
 print_last_word(sorted_words)
-sorted_words = ex25.sort_sentence(sentence)
-prin sorted_words
+# bug: sorted_words = ex25.sort_sentence(sentence)
+sorted_words = sort_sentence(sentence)
+# bug: prin sorted_words
+print(sorted_words)
+# bug: print_irst_and_last(sentence)
+print_first_and_last(sentence)
 
-print_irst_and_last(sentence)
-
-   print_first_a_last_sorted(senence)
+# bug:   print_first_a_last_sorted(senence)
+print_first_and_last_sorted(sentence)
