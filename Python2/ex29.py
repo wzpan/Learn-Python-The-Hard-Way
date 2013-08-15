5c59f999ae36f7f6a97ab8409719268d4136ade0
Joseph Pan <cs.wzpan@gmail.com>
2013-08-15 16:49:15 +0000

    add ex29

---
+++ Python2/ex29.py
@@ -0,0 +1,21 @@
+#!/bin/python2
+# -*- coding: utf-8 -*-
+
+# ex29: What If
+
+people = 20
+cats = 30
+dogs = 15
+
+
+if people < cats:
+    print "Too many cats! The world is doomed!"
+
+if people > cats:
+    print "Not many cats! The world is saved!"
+
+if people < dogs:
+    print "The world is drooled on!"
+
+if people > dogs:
+    print "The world is dry!"
