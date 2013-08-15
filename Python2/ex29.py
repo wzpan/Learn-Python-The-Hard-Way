498a0086aefc893a59602cbd94deaaab20c3801a
Joseph Pan <cs.wzpan@gmail.com>
2013-08-15 17:03:47 +0000

    put other boolean expressions

--- Python2/ex29.py
+++ Python2/ex29.py
@@ -19,3 +19,26 @@ if people < dogs:
 
 if people > dogs:
     print "The world is dry!"
+
+dogs += 5
+
+if people >= dogs:
+    print "People are greater than or equal to dogs."
+
+if people <= dogs:
+    print "People are less than or equal to dogs."
+
+if people == dogs:
+    print "People are dogs."
+
+if (dogs < cats) and (people < cats):
+    print "Cats are more than people and dogs. People are scared by cats!"    
+
+if (dogs < cats) and not (people < cats):
+    print "Cats are more than dogs. Mice are living a hard life!"
+    
+if (dogs == cats) or (cats < 10):
+    print "Cats are fighting against dogs! Mice are happy!"
+
+if cat != 0: 
+    print "Cats are still exist. Mice cannot be too crazy."
