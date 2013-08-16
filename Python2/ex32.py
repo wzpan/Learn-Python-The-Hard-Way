ae03c61e62a05dd3bc9532678f84569c7c99e075
Joseph Pan <cs.wzpan@gmail.com>
2013-08-16 17:49:12 +0000

    avoid for-loop

--- Python2/ex32.py
+++ Python2/ex32.py
@@ -25,11 +25,8 @@ for i in change:
 # we can also build lists, first start with an empty one
 elements = []
 
-# then use the range function to do 0 to 5 counts
-for i in range(0, 6):
-    print "Adding %d to the list." % i
-    # append is a function that lists understand
-    elements.append(i)
+# then use the range function to generate a list
+elements = range(0, 6)
     
 # now we can print them out too
 for i in elements:
