676559d6c5a04dbafbbbcef14718e90e2e8cd4f0
Joseph Pan <cs.wzpan@gmail.com>
2013-08-16 12:30:01 +0000

    add ex30

---
+++ Python2/ex30.py
@@ -0,0 +1,28 @@
+#!/bin/python2
+# -*- coding: utf-8 -*-
+
+# ex30: Else and If
+
+people = 30
+cars = 40
+buses = 15
+
+
+if cars > people:
+    print "We should take the cars."
+elif cars < people:
+    print "We should not take the cars"
+else:
+    print "We can't decide."
+
+if buses > cars:
+    print "That's too many buses."
+elif buses < cars:
+    print "Maybe we could take the buses."
+else:
+    print "We still can't decide."
+
+if people > buses:
+    print "Alright, let's just take the buses."
+else:
+    print "Fine, let's stay home then."
