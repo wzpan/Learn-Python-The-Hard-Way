66f225a680e8cf5bbc36e0ea79c41ac8759756c0
Joseph Pan <cs.wzpan@gmail.com>
2013-08-16 17:01:25 +0000

    add ex31

---
+++ Python3/ex31.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+# ex31: Making Decisions
+
+print("You enter a dark room with two doors. Do you go through door #1 or door #2?")
+
+door = input("> ")
+
+if door == "1":
+    print("There's a giant bear here eating a cheese cake. What do you do?")
+    print("1. Take the cake.")
+    print("2. Scream at the bear.")
+
+    bear = input("> ")
+
+    if bear == "1":
+        print("The bear eats your face off. Good job!")
+    elif bear == "2":
+        print("The bear eats your legs off. Good job!")
+    else:
+        print("Well, doing %s is probably better. Bear runs away." % bear)
+
+elif door == "2":
+    print("You stare into the endless abyss at Cthulhu's retina.")
+    print("1. Blueberries.")
+    print("2. Yellow jacket clothespins.")
+    print("3. Understanding revolvers yelling melodies.")
+
+    insanity = input("> ")
+
+    if insanity == "1" or insanity == "2":
+        print("Your body survives powered by a mind of jello. Good job!")
+    else:
+        print("The insanity rots you ")
+
+else:
+    print("You stumble around and fell on a knife and die. Good job!")
