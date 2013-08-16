1ffcf4ac244b6a922ef3d8357389934ca3ac9c24
Joseph Pan <cs.wzpan@gmail.com>
2013-08-16 19:26:05 +0000

    add the step variable

--- Python2/ex33.py
+++ Python2/ex33.py
@@ -3,20 +3,19 @@
 
 # ex33: While Loops
 
-def createNumbers(max):
+def createNumbers(max, step):
     i = 0
     numbers = []
     while i < max:
         print "At the top i is %d" % i
         numbers.append(i)
 
-        i = i + 1
+        i = i + step
         print "Numbers now: ", numbers
         print "At the bottom i is %d" % i
     return numbers
 
-numbers = createNumbers(10)
-
+numbers = createNumbers(10, 2)
 
 print "The numbers: "
 
