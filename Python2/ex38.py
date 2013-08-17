1ad9497075ae806f49dbad1cb90f92186bdcd4d3
Joseph Pan <cs.wzpan@gmail.com>
2013-08-17 19:18:00 +0000

    add ex38

---
+++ Python2/ex38.py
@@ -0,0 +1,27 @@
+#!/bin/python2
+# -*- coding: utf-8 -*-
+
+# ex38: Doing Things To Lists
+
+ten_things = "Apples Oranges Crows Telephone Light Sugar"
+
+print "Wait there's not 10 things in that list, let's fix that."
+
+stuff = ten_things.split(' ')
+more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Girl", "Boy"]
+
+while len(stuff) != 10:
+    next_one = more_stuff.pop()
+    print "Adding: ", next_one
+    stuff.append(next_one)
+    print "There's %d items now." % len(stuff)
+
+print "There we go: ", stuff
+
+print "Let's do some things with stuff."
+
+print stuff[1]
+print stuff[-1]  # whoa! fancy
+print stuff.pop()
+print ' '.join(stuff)  # what? cool!
+print '#'.join(stuff[3:5])  # super stellar!
