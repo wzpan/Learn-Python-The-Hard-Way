5155163c794cddaed07c8c7916f832843de5d3f3
Joseph Pan <cs.wzpan@gmail.com>
2013-08-18 16:22:07 +0000

    add ex43

---
+++ Python3/ex43.py
@@ -0,0 +1,73 @@
+#!/bin/python3
+
+# ex43: Basic Object-Oriented Analysis and Design
+
+# Class Hierarchy
+# * Map
+#   - next_scene
+#   - opening_scene
+# * Engine
+#   - play
+# * Scene
+#   - enter
+#   * Death
+#   * Central Corridor
+#   * Laser Weapon Armory
+#   * The Bridge
+#   * Escape Pod
+
+class Map(object):
+
+    def __init__(self, start_scene):
+        pass
+
+    def next_scene(self, scene_name):
+        pass
+
+    def opening_scene(self):
+        pass
+
+
+class Engine(object):
+    
+    def __init__(self, scene_map):
+        pass
+
+    def play(self):
+        pass
+
+
+class Scene(object):
+    
+    def enter(self):
+        pass
+
+class Death(Scene):
+    
+    def enter(self):
+        pass
+
+class CentralCorridor(Scene):
+
+    def enter(self):
+        pass
+
+class LaserWeaponArmory(Scene):
+
+    def enter(self):
+        pass
+
+class TheBridge(Scene):
+
+    def enter(self):
+        pass
+
+class EscapePod(Scene):
+
+    def enter(self):
+        pass
+
+
+a_map = Map('central_corridor')
+a_game = Engine(a_map)
+a_game.play()
