66682b252812fc6cb796647a202845ff2d56b7df
Joseph Pan <cs.wzpan@gmail.com>
2013-08-18 20:46:25 +0000

    alter before of after

--- Python2/ex44.py
+++ Python2/ex44.py
@@ -5,16 +5,18 @@
 
 class Parent(object):
 
-    def override(self):
-        print "PARENT override()"
+    def altered(self):
+        print "PARENT altered()"
 
 class Child(Parent):
 
-    def override(self):
-        print "CHILD override()"
+    def altered(self):
+        print "CHILD, BEFORE PARENT altered()"
+        super(Child, self).altered()
+        print "CHILD, AFTER PARENT altered()"
 
 dad = Parent()
 son = Child()
 
-dad.override()
-son.override()
+dad.altered()
+son.altered()
