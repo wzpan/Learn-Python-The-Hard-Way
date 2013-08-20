7476869f056d5fbda4efba07c742f32c5783c20e
Joseph Pan <cs.wzpan@gmail.com>
2013-08-20 15:38:33 +0000

    add a link back to /hello

--- Python2/ex50/gothonweb/bin/app.py
+++ Python2/ex50/gothonweb/bin/app.py
@@ -1,7 +1,7 @@
 import web
 
 urls = (
-    '/', 'index'
+    '/hello', 'index'
     )
 
 app = web.application(urls, globals())
@@ -10,12 +10,11 @@ render = web.template.render('templates/')
 
 class index:
     def GET(self):
-        form = web.input(greet=None, name="Nobody")
-        if form.greet:
-            greeting = "%s, %s" % (form.greet, form.name)
-            return render.index(greeting=greeting)
-        else:
-            return "ERROR: greet is required."
+        return render.hello_form()
+    def POST(self):
+        form = web.input(name="Nobody", greet="Hello")
+        greeting = "%s, %s" % (form.greet, form.name)
+        return render.index(greeting = greeting)
 
 if __name__ == "__main__":
     app.run()
