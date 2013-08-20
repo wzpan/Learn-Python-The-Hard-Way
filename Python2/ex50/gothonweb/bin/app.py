4d208c7fd7bd4b13b1c30b6625d6486a80146680
Joseph Pan <cs.wzpan@gmail.com>
2013-08-20 20:42:25 +0000

    add a upload form to upload and view image.

--- Python2/ex50/gothonweb/bin/app.py
+++ Python2/ex50/gothonweb/bin/app.py
@@ -1,20 +1,51 @@
-import web
+import web, datetime, os
+
 
 urls = (
-    '/hello', 'index'    
+    '/', 'Index',
+    '/hello', 'SayHello',
+    '/image', 'Upload',
     )
 
 app = web.application(urls, globals())
 
 render = web.template.render('templates/', base="layout")
 
-class index:
+
+class Index:
+    def GET(self):
+        return render.index()
+
+
+class SayHello:
     def GET(self):
         return render.hello_form()
     def POST(self):
         form = web.input(name="Nobody", greet="Hello")
         greeting = "%s, %s" % (form.greet, form.name)
-        return render.index(greeting = greeting)
+        return render.hello(greeting = greeting)
+
+
+class Upload:
+    ''' using cgi to upload and show image '''
+    def GET(self):
+        return render.upload_form()
+    def POST(self):
+        form = web.input(myfile={})
+        # web.debug(form['myfile'].file.read())
+        # get the folder name
+        upload_time = datetime.datetime.now().strftime("%Y-%m-%d")
+        # create the folder
+        folder = os.path.join('./static', upload_time)
+        if not os.access(folder, 1):
+            os.mkdir(folder)
+        # get the file name
+        filename = os.path.join(folder, form['myfile'].filename)
+        with open(filename, 'wb') as f:
+            f.write(form['myfile'].file.read())
+            f.close()
+        return render.show(filename = filename)
+        
 
 if __name__ == "__main__":
     app.run()
