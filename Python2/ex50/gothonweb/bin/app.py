f5e66ecb1d0e5b119c65bb0ea75832bbe1f5d2a1
Joseph Pan <cs.wzpan@gmail.com>
2013-08-20 12:58:36 +0000

    get two parameter on the URL

--- Python2/ex50/gothonweb/bin/app.py
+++ Python2/ex50/gothonweb/bin/app.py
@@ -10,10 +10,10 @@ render = web.template.render('templates/')
 
 class index:
     def GET(self):
-        form = web.input(name="Nobody")
+        form = web.input(greet="Hello", name="Nobody")
         words = "Yet another website engine"
-        greeting = "Hello %s" % form.name
-        return render.index(greeting = greeting)
+        greeting = "%s, %s" % (form.greet, form.name)
+        return render.index(greeting=greeting)
         # return render.foo(word = words)
 
 if __name__ == "__main__":
