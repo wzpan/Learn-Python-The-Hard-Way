3af35c38dab0f228666b1f67407fbd753b1dc345
Joseph Pan <cs.wzpan@gmail.com>
2013-08-20 12:55:29 +0000

    using web.input to get data from the browser

--- Python2/ex50/gothonweb/bin/app.py
+++ Python2/ex50/gothonweb/bin/app.py
@@ -10,10 +10,11 @@ render = web.template.render('templates/')
 
 class index:
     def GET(self):
+        form = web.input(name="Nobody")
         words = "Yet another website engine"
-        greeting = "Hello world"
-        #return render.index(greeting = greeting)
-        return render.foo(word = words)
+        greeting = "Hello %s" % form.name
+        return render.index(greeting = greeting)
+        # return render.foo(word = words)
 
 if __name__ == "__main__":
     app.run()
