9affb6b217e85662324a26b8501d7dafce95919f
Raphael Thomalla <thomilla.net@web.de>
2014-01-08 17:10:43 +0100

    Update ex12.py
    Hi, same here... input or eval...

--- Python3/ex12.py
+++ Python3/ex12.py
@@ -1,7 +1,7 @@
 #!/bin/python3
 
 # ex12: Prompting People
-
+# exchange raw_input with input() or eval(input())
 age = raw_input("How old are you?")
 height = raw_input("How tall are you? ")
 weight = raw_input("How much do you weigh? ")
